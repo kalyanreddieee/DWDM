30617c5f9a6f800a50a6c08bdeae388077a5717e
kalyan reddy <113888414+kalyanreddieee@users.noreply.github.com>
2023-08-11 12:31:34 +0530

    Delete K-means.R

--- Png Files/K-means.R
+++
@@ -1,7 +0,0 @@
-data(iris) 
-x <- iris[,c("Sepal.Length", "Sepal. Width", "Petal. Length", "Petal. Width")] 
-kmeans_model <- kmeans(x, centers = 3) 
-kmeans_model 
-library(ggplot2) 
-ggplot(iris,aes(x=Sepal.Length,y=Sepal.Width,color=factor(kmeans_model$cluste))
-) +geom_point() 
