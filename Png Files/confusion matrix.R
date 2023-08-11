e8ee3bcc4e390bb81d50d9d421461814ef040638
kalyan reddy <113888414+kalyanreddieee@users.noreply.github.com>
2023-08-11 12:25:59 +0530

    Delete confusion matrix.R

--- Png Files/confusion matrix.R
+++
@@ -1,7 +0,0 @@
-set.seed(123)
-data<-data.frame(Actual=sample(c("True","False"),100,.replace=TRUE),
-                 prediction=sample(c("True","False"),100,replace=TRUE)
-)
-table(data$prediction,data$Actual)
-                 
-
